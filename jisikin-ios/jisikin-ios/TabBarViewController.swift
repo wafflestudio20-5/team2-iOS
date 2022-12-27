6b0a70f870a178e2324c812626a09013eca88b79
김령교 <horizonspluto@gmail.com>
2022-12-27 23:44:23 +0900

    건의사항을 반영함

--- jisikin-ios/jisikin-ios/TabBarViewController.swift
+++ jisikin-ios/jisikin-ios/TabBarViewController.swift
@@ -16,22 +16,21 @@ class TabBarViewController: UITabBarController {
         let questionVC = QuestionViewController()
         let myVC = MyViewController()
         
-        answerVC.title = "답변하기"
-        questionVC.title = "질문하기"
-        myVC.title = "MY"
-        
         let answerNav = UINavigationController(rootViewController: answerVC)
         let questionNav = UINavigationController(rootViewController: questionVC)
         let myNav = UINavigationController(rootViewController: myVC)
         
         setViewControllers([answerNav, questionNav, myNav], animated: false)
         
+        tabBar.items![0].title = "답변하기"
         tabBar.items![0].selectedImage = UIImage (systemName: "a.square.fill")
         tabBar.items![0].image = UIImage (systemName: "a.square")
         
+        tabBar.items![1].title = "질문하기"
         tabBar.items![1].selectedImage = UIImage (systemName: "q.square.fill")
         tabBar.items![1].image = UIImage (systemName: "q.square")
         
+        tabBar.items![2].title = "MY"
         tabBar.items![2].selectedImage = UIImage (systemName: "person.circle.fill")
         tabBar.items![2].image = UIImage (systemName: "person.circle")
         
