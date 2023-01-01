61b41aebce90ca74a58823235bf05ae31191e8ce
Heeju <jeenakit@naver.com>
2023-01-01 15:56:33 +0900

    ui 기본 구성
    이미지 삽입 버튼, 태그 텍스트 필드 키보드 위에 추가하는 거 필요

--- jisikin-ios/jisikin-ios/QuestionViewController.swift
+++ jisikin-ios/jisikin-ios/QuestionViewController.swift
@@ -7,24 +7,132 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController {
+extension UITextField {
+    func setPlaceHolderColor(_ placeholderColor: UIColor) {
+        attributedPlaceholder = NSAttributedString(
+            string: placeholder ?? "",
+            attributes: [
+                .foregroundColor: placeholderColor,
+                .font: font
+            ].compactMapValues { $0 }
+        )
+    }
+}
 
+class QuestionViewController: UIViewController, UITextFieldDelegate {
+    
+    lazy var rightButton: UIBarButtonItem = {
+        let btn = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneQuestion(_:)))
+        return btn
+    }()
+    
+    lazy var plusImageButton: UIButton = {
+        let btn = UIButton()
+    // 키보드 위에 태그 다는 텍스트필드랑 이미지 추가 버튼 다는 거부터 작업하면 됨
+        return btn
+    }()
+    
+    lazy var titleField: UITextField = {
+        let field = UITextField()
+        
+        field.borderStyle = .none
+        field.placeholder = "제목"
+        field.font = .systemFont(ofSize: 25)
+        field.setPlaceHolderColor(UIColor.lightGray)
+        field.backgroundColor = UIColor.white
+        field.delegate = self
+        
+        return field
+    }()
+    
+    let textViewPlaceHolder = "무엇이 궁금한가요?"
+    
+    lazy var accessoryView: UIView = {
+        return UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 72))
+    }()
+    
+    lazy var contentView: UITextView = {
+        let view = UITextView()
+        view.inputAccessoryView = accessoryView
+        view.font = .systemFont(ofSize: 20)
+        view.text = textViewPlaceHolder
+        view.textColor = UIColor.lightGray
+        view.delegate = self
+        
+        return view
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.titleField.becomeFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        setNavigationBar()
+        setLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setNavigationBar() {
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+        
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.topItem?.title = "질문하기"
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = rightButton
     }
-    */
+    
+    private func setLayout() {
+        view.addSubview(titleField)
+        view.addSubview(contentView)
+        
+        titleField.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10.0),
+            titleField.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10.0),
+            titleField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10.0),
+            contentView.leadingAnchor.constraint(equalTo: titleField.leadingAnchor),
+            contentView.rightAnchor.constraint(equalTo: titleField.rightAnchor),
+            contentView.topAnchor.constraint(equalTo: titleField.bottomAnchor, constant: 20),
+            contentView.heightAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapContentView(_:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func didTapContentView(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @objc private func doneQuestion(_ sender: Any) {
+            
+    }
+}
 
+extension QuestionViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == textViewPlaceHolder {
+            textView.text = nil
+            textView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            textView.text = textViewPlaceHolder
+            textView.textColor = .lightGray
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let inputString = text.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard let oldString = textView.text, let newRange = Range(range, in: oldString) else { return true }
+        let newString = oldString.replacingCharacters(in: newRange, with: inputString).trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        return true
+    }
 }
