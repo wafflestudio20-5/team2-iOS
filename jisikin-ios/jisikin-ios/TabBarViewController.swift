abeb336ee19f2ee8d25389fe6dfa6e4e16e46f8d
Chaehyun Park <c.park@snu.ac.kr>
2023-01-01 14:14:58 +0900

    navigationBarHide
    TabBarViewController의 navigationBar가 표시되지 않도록 수정

--- jisikin-ios/jisikin-ios/TabBarViewController.swift
+++ jisikin-ios/jisikin-ios/TabBarViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
