f7227835241c3b3d60236b57c7b0a31288a36c81
박정헌 <impri@naver.om>
2023-01-09 23:33:06 +0900

    질문상세 화면 서버 연결

---
+++ jisikin-ios/jisikin-ios/QuestionAnswerUsecase.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionAnswerUsecase.swift
+//  jisikin-ios
+//
+//  Created by 박정헌 on 2023/01/09.
+//
+
+import Foundation
---
+++ jisikin-ios/jisikin-ios/QuestionDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionDetailViewModel.swift
+//  jisikin-ios
+//
+//  Created by 박정헌 on 2023/01/09.
+//
+
+import Foundation
